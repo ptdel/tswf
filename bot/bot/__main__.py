1934d7370eb35815d2737a86dda5c2fe448c9869
Patrick Delaney <pthomasdelaney@gmail.com>
2020-01-05 15:24:47 -0500

    actually removing bot/

--- bot/bot/__main__.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from bot import BotFactory
-from twisted.internet import reactor
-from secure import CtxFactory
-
-if __name__ == "__main__":
-    f = BotFactory("#dailyprog", "")
-    reactor.connectSSL(environ["IRC_SERVER"], 6697, f, CtxFactory())
-    reactor.run()
